bc828fb69b3831d73992a846b2a897526ceac9f7
Carlos Osorio <hacybeyker@gmail.com>
2020-11-10 09:28:15 -0500

    commit inicial

---
+++ payment/src/main/java/com/rimac/core/extensions/Extensions.kt
@@ -0,0 +1,5 @@
+package com.rimac.core.extensions
+
+fun Double.calculateTotal(amount: Int): Double {
+    return this * amount
+}
---
+++ payment/src/main/java/com/rimac/core/validations/Validations.kt
@@ -0,0 +1,5 @@
+package com.rimac.core.validations
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
